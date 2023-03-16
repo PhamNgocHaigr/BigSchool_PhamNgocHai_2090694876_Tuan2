98939817417e14e34e22261216055e99830dece3
ASUS <ASUS@DESKTOP-SOG8JRM>
2023-03-16 11:02:36 +0700

    Add project files.

---
+++ BigSchool_PhamNgocHai_2090694876/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BigSchool_PhamNgocHai_2090694876.MvcApplication" Language="C#" %>
---
+++ BigSchool_PhamNgocHai_2090694876/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_PhamNgocHai_2090694876.Startup))]
+namespace BigSchool_PhamNgocHai_2090694876
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
